cbd2d2142024e8f5a8c84ead95fdd2d5f266bdac
RubashkaGuy <jerrethkerman1@gmail.com>
2024-11-21 09:50:20 +0300

    final update?

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,1 +1,37 @@
 console.log('ok');
+
+
+// Находим элементы
+const toggleButton = document.getElementById('pc__button');
+const menuSection = document.getElementById('mb');
+
+// Добавляем обработчик события для кнопки
+toggleButton.addEventListener('click', () => {
+    // Проверяем, видима ли секция, и переключаем состояние
+    if (menuSection.style.display === 'none' || menuSection.style.display === '') {
+        menuSection.style.display = 'grid'; // Показываем секцию
+    } else {
+        menuSection.style.display = 'none'; // Скрываем секцию
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollUp = document.querySelector(".scrollup");
+    const header = document.querySelector("header"); // Обязательно убедитесь, что секция имеет тег <header>
+
+    // Показать кнопку, когда прокрутили вниз на 300px
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 10) {
+            scrollUp.style.display = "block";
+        } else {
+            scrollUp.style.display = "none";
+        }
+    });
+
+    // Прокрутка к началу страницы
+    scrollUp.addEventListener("click", (e) => {
+        e.preventDefault(); // Предотвращаем переход по ссылке
+        header.scrollIntoView({ behavior: "smooth" });
+    });
+});
